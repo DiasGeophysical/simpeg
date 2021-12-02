fix bug that was creating the regularization to call misfit's 2nd derivative.

SimPEG/dias/objective_function.py
@@ -6,8 +6,7 @@ import shutil
 import numpy as np
 from dask.distributed import Future, get_client, Client
 
-
-def dask_call(self, m, f=None):
+def dias_call(self, m, f=None):
     fcts = []
     multipliers = []
     for i, phi in enumerate(self):
@@ -42,10 +41,10 @@ def dask_call(self, m, f=None):
         ).squeeze()
 
 
-ComboObjectiveFunction.__call__ = dask_call
+ComboObjectiveFunction.__call__ = dias_call
 
 
-def dias_deriv(self, residual):
+def dias_deriv(self, m, f=None):
     """
     First derivative of the composite objective function is the sum of the
     derivatives of each objective function in the list, weighted by their
@@ -54,7 +53,7 @@ def dias_deriv(self, residual):
     :param numpy.ndarray m: model
     :param SimPEG.Fields f: Fields object (if applicable)
     """
-
+    print("\n\n In obj first? \n\n")
     g = []
     multipliers = []
     for i, phi in enumerate(self):
@@ -63,23 +62,37 @@ def dias_deriv(self, residual):
             continue
         else:
 
-            fct = objfct.deriv(residual)
-            
-            fct = da.multiply(multiplier, fct)
-            
-            g += [fct]
+            if f is not None and objfct._has_fields:
+                fct = objfct.deriv(m, f=f[i])
+            else:
+                fct = objfct.deriv(m)
+
+            if isinstance(fct, Future):
+                future = self.client.compute(
+                    self.client.submit(da.multiply, multiplier, fct)
+                )
+                g += [future]
+            else:
+                g += [fct]
 
             multipliers += [multiplier]
 
-    return np.sum(
-        np.r_[multipliers][:, None] * np.vstack(g), axis=0
-    ).squeeze()
+    if isinstance(g[0], Future):
+        big_future = self.client.submit(
+            da.sum, self.client.submit(da.vstack, g), axis=0
+        ).result()
+        return self.client.compute(big_future).result()
+
+    else:
+        return np.sum(
+            np.r_[multipliers][:, None] * np.vstack(g), axis=0
+        ).squeeze()
 
 
 ComboObjectiveFunction.deriv = dias_deriv
 
 
-def dias_deriv2(self, v=None):
+def dias_deriv2(self, m, v=None, f=None):
     """
     Second derivative of the composite objective function is the sum of the
     second derivatives of each objective function in the list, weighted by
@@ -97,18 +110,35 @@ def dias_deriv2(self, v=None):
         if multiplier == 0.0:  # don't evaluate the fct
             continue
         else:
-            fct = objfct.deriv2(v)
 
-            fct = da.multiply(multiplier, fct)
-            H += [fct]
+            # if f is not None and objfct._has_fields:
+            #     fct = objfct.deriv2(m, v, f=f[i])
+            # else:
+                # print('[info] doing derive no fields')
+            fct = objfct.deriv2(m, v)
+
+            if isinstance(fct, Future):
+                future = self.client.compute(
+                    self.client.submit(da.multiply, multiplier, fct)
+                )
+                H += [future]
+            else:
+                H += [fct]
 
             multipliers += [multiplier]
 
-    phi_deriv2 = 0
-    for multiplier, h in zip(multipliers, H):
-        phi_deriv2 += multiplier * h
+    if isinstance(H[0], Future):
+        big_future = self.client.submit(
+            da.sum, self.client.submit(da.vstack, H), axis=0
+        ).result()
+        return self.client.compute(big_future).result()
 
-    return phi_deriv2
+    else:
+        phi_deriv2 = 0
+        for multiplier, h in zip(multipliers, H):
+            phi_deriv2 += multiplier * h
+
+        return phi_deriv2
 
 
 ComboObjectiveFunction.deriv2 = dias_deriv2

SimPEG/dias/worker_utils/server_inversion.py
@@ -168,7 +168,7 @@ def initialize(tile_config):
     local_misfit = data_misfit.L2DataMisfit(
         data=data_object, simulation=simulation, model_map=local_map
     )
-    local_misfit.W = 1 / local_survey.std
+    local_misfit.W = 1 / uncert
 
     print('completed...', local_survey.nD, local_misfit.simulation.mesh.nC, local_map.local_active.sum())
 
@@ -218,6 +218,7 @@ def getDeriv2(inputs, local_misfit, fields, model):
 
     v = np.asarray(inputs['vector'])
     print('[INFO] made V from json')
+    
     # convert to local model size
     if local_misfit.model_map is not None:
         vec = local_misfit.model_map @ model
@@ -231,7 +232,6 @@ def getDeriv2(inputs, local_misfit, fields, model):
         fields, Ainv = local_misfit.simulation.fields(vec, return_Ainv=True)
         local_misfit.simulation.Ainv = Ainv
 
-    
     jvec = local_misfit.simulation.dias_Jvec(vec, v, f=fields)
     print('[INFO] made jvec')
     w_jvec = local_misfit.W.diagonal() ** 2.0 * jvec
@@ -376,7 +376,10 @@ def worker_server():
                         outputs = {}
 
                         # check request type and execute
-                        if request_type == 'init':                            
+                        if request_type == 'init':
+
+                            # reset all the global variables
+                            LOCAL_MISFIT, FIELDS, MODEL, RESIDUAL = None, None, None, None
                             # call initialization
                             print("\n\n here in init \n\n")
                             
@@ -387,16 +390,17 @@ def worker_server():
 
                             print("\n\n completed \n")
 
-                        elif request_type == 'dpred':                            
+                        elif request_type == 'dpred':
                             # call dpred
                             print("\n\n here in dpred \n\n")
+                            MODEL = None
                             residual, FIELDS, MODEL = getPredictedData(params, LOCAL_MISFIT)
                             phi_d = 0.5 * np.vdot(residual, residual)
                             outputs["residual"] = phi_d
                             RESIDUAL = residual
                             print("\n\n completed residuals \n\n")
 
-                        elif request_type == 'jtvec':                           
+                        elif request_type == 'jtvec':
                             # call jtvec
                             print("\n\n here in jtvec \n\n")
                             wtw_d = LOCAL_MISFIT.W.diagonal() ** 2.0 * RESIDUAL
@@ -404,7 +408,7 @@ def worker_server():
                             outputs["jtvec"] = out_data.tolist()
                             print("\n\n completed jtvec \n\n")
                         
-                        elif request_type == 'deriv2':                            
+                        elif request_type == 'deriv2':
                             # call jvec
                             print("\n\n here in deriv2 \n\n")
                             out_data, FIELDS = getDeriv2(params, LOCAL_MISFIT, FIELDS, MODEL)
@@ -412,7 +416,7 @@ def worker_server():
                             outputs["deriv2"] = out_data.tolist()
                             print("\n\n assigned deriv2 \n\n")
 
-                        elif request_type == 'clean':                            
+                        elif request_type == 'clean':
                             # call dpred
                             outputs["cleaned"] = clearLocalSimulation(params)
 